138f0c860c4cb9ab160bcf89699c6bc4d51568e8
Jeroen Ooms <jeroen.ooms@incentro.com>
2014-12-04 15:43:49 +0100

    First stage

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-theFlexGrid
+The Flex Grid
 ===========
 
-Modern lightweight CSS grid using flexbox. Because we can!
+The Flex Grid is an lightweight grid build with CSS3 feature flexbox. Just because we can! The grid is made for modern browsers, there is no support for dinosaurs.
+
+<h2>The Grid</h2>
+The Flex Grid has 12 columns, just like Bootstrap. Each columns
+
+
+```
+<div class="grid">
+    <div class="row">
+        <div class="col-12">
+            <div class="box"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <div class="box"></div>
+        </div>
+        <div class="col-9">
+            <div class="box"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="box"></div>
+        </div>
+        <div class="col-6">
+            <div class="box"></div>
+        </div>
+    </div>
+</div>
+```
+
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict';
+// generated on 2014-11-29 using generator-gulp-webapp 0.1.0
+
+var gulp = require('gulp');
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+    return gulp.src('app/styles/*.scss')
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10
+        }))
+        .pipe($.autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('.tmp/styles'))
+        .pipe($.size());
+});
+
+gulp.task('html', ['styles'], function () {
+    var cssFilter = $.filter('**/*.css');
+
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+        .pipe(cssFilter)
+        //.pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe($.highlight())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', ['html']);
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('connect', function () {
+    var connect = require('connect');
+    var app = connect()
+        .use(require('connect-livereload')({ port: 35729 }))
+        .use(connect.static('app'))
+        .use(connect.static('.tmp'))
+        .use(connect.directory('app'));
+
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:9000');
+        });
+});
+
+gulp.task('serve', ['connect', 'styles'], function () {
+    require('opn')('http://localhost:9000');
+});
+
+gulp.task('watch', ['connect', 'serve'], function () {
+    var server = $.livereload();
+
+    // watch for changes
+
+    gulp.watch([
+        'app/*.html',
+        '.tmp/styles/**/*.css',
+    ]).on('change', function (file) {
+        server.changed(file.path);
+    });
+
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+});
